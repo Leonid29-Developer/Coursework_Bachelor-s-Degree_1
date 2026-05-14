e54e84110106f2ce0c9a3c832b5a63a79702826e
Leonid29-Developer <job@leonid29-developer.ru>
2026-05-15 00:12:48 +0300

    Создание функции дешифрования текстового сообщения с помощью кода Цезаря (сдвиг влево) в зависимости от значения sid

--- cypher.py
+++ cypher.py
@@ -45,3 +45,38 @@ def encrypt(text, sid=70220069):
             encrypted.append(char)
 
     return ''.join(encrypted)
+
+
+def decrypt(text, sid=70220069):
+    """
+    Дешифрует текстовое сообщение с помощью кода Цезаря (сдвиг влево).
+
+    Параметры:
+    text (str): Исходное текстовое сообщение для шифрования
+    sid (int): ID от значения которого будет вычисляться размер сдвига
+        (по умолчанию 70220069)
+
+    Возвращает:
+    str: Зашифрованное сообщение.
+    """
+
+    decrypted = []
+    # Длина массива символов
+    array_length = len(CHAR_ARRAY)
+    # Размер сдвига
+    shift = sid % 11
+
+    print(f"Сдвиг: {shift}")
+
+    for char in text:
+        if char in CHAR_ARRAY:
+
+            index = CHAR_ARRAY.index(char)
+            new_index = (index - shift) % array_length
+            decrypted.append(CHAR_ARRAY[new_index])
+
+        else:
+            # Оставить символы, не входящие в массив, без изменений
+            decrypted.append(char)
+
+    return ''.join(decrypted)

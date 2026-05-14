0734e18266c8392f0874a2d093e4c9da51b111e6
Leonid29-Developer <job@leonid29-developer.ru>
2026-05-14 23:51:52 +0300

    Создание функции шифрования текстового сообщения с помощью кода Цезаря (сдвиг вправо) в зависимости от значения sid

--- cypher.py
+++ cypher.py
@@ -1,12 +1,47 @@
 import string
 
 # Создание массива символов в заданном порядке
-CharArray = (
-    '0123456789' +  # Цифры
-    'абвгдеёжзийклмнопрстуфхцчшщъыьэюя' +  # Малые русские
-    'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' +  # Большие русские
-    'abcdefghijklmnopqrstuvwxyz' +  # Малые латинские
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  # Большие латинские
-    string.punctuation +  # Пунктуация
-    ' '  # Пробел
+CHAR_ARRAY = (
+        '0123456789' +  # Цифры
+        'абвгдеёжзийклмнопрстуфхцчшщъыьэюя' +  # Малые русские
+        'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ' +  # Большие русские
+        'abcdefghijklmnopqrstuvwxyz' +  # Малые латинские
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ' +  # Большие латинские
+        string.punctuation +  # Пунктуация
+        ' '  # Пробел
 )
+
+
+def encrypt(text, sid=70220069):
+    """
+    Шифрует текстовое сообщение с помощью кода Цезаря (сдвиг вправо)
+
+    Параметры:
+    text (str): Исходное текстовое сообщение для шифрования
+    sid (int): ID от значения которого будет вычисляться размер сдвига
+        (по умолчанию 70220069)
+
+    Возвращает:
+    str: Зашифрованное сообщение.
+    """
+
+    encrypted = []
+    # Длина массива символов
+    array_length = len(CHAR_ARRAY)
+    # Размер сдвига
+    shift = sid % 11
+
+    print(f"Сдвиг: {shift}")
+
+    for char in text:
+        if char in CHAR_ARRAY:
+
+            index = CHAR_ARRAY.index(char)
+            new_index = (index + shift) % array_length
+            encrypted.append(CHAR_ARRAY[new_index])
+
+        else:
+            # Оставить символы, не входящие в массив, без изменений
+            encrypted.append(char)
+
+    return ''.join(encrypted)
